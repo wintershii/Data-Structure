7b20feb7d0288f245f02d9b348c38451c9a87465
wintershii <wintershii@163.com>
2018-10-08 17:36:27 +0800

    prac2

---
+++ 课程练习/Node.java
@@ -0,0 +1,33 @@
+class Node{
+    int x;
+    int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" +
+                "x=" + x +
+                ", y=" + y +
+                ')';
+    }
+}

b6f8b20114e07bf7bcac822a65f02756730b3834
wintershii <wintershii@163.com>
2018-11-11 21:34:39 +0800

    ubuntu first

---
+++ java/src/com/winter/tree/BinaryTree.java
@@ -0,0 +1,193 @@
+package com.winter.tree;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+/**
+ *  二叉树 建立 递归与非递归遍历
+ */
+public class BinaryTree {
+    //二叉树跟节点
+    private TreeNode root = null;
+
+    public BinaryTree() {
+    }
+
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.root = bt.createBinaryTree();
+        System.out.println("前序遍历:");
+        preOrder(bt.root);
+        System.out.println();
+        System.out.println("中序遍历:");
+        inOrder(bt.root);
+        System.out.println();
+        System.out.println("后序遍历:");
+        aftOrder(bt.root);
+        System.out.println();
+
+        System.out.println("非递归前序遍历:");
+        nonRecPreOrder(bt.root);
+        System.out.println("非递归中序遍历:");
+        nonRecInOrder(bt.root);
+        System.out.println("非递归后序遍历:");
+        nonRecAftOrder(bt.root);
+
+
+    }
+
+    /**
+     * 递归构造二叉树
+     * @return 二叉树根节点
+     */
+    public TreeNode createBinaryTree() {
+        TreeNode root;
+        System.out.print("请输入此数据:(输入值为-1时为空)");
+        Scanner scan = new Scanner(System.in);
+        int data = scan.nextInt();
+        if (data == -1) {
+            return null;
+        } else {
+            root = new TreeNode();
+            root.data = data;
+            root.leftChild = createBinaryTree();
+            root.rightChild = createBinaryTree();
+        }
+        return root;
+    }
+
+    /**
+     * 前序遍历
+     * @param root
+     */
+    public static void preOrder(TreeNode root) {
+        if (root != null) {
+            System.out.print(root);
+            preOrder(root.leftChild);
+            preOrder(root.rightChild);
+        }
+    }
+
+    /**
+     * 中序遍历
+     * @param root
+     */
+    public static void inOrder(TreeNode root) {
+        if (root != null) {
+            inOrder(root.leftChild);
+            System.out.print(root);
+            inOrder(root.rightChild);
+        }
+    }
+
+    /**
+     * 后序遍历
+     * @param root
+     */
+    public static void aftOrder(TreeNode root) {
+        if (root != null) {
+            aftOrder(root.leftChild);
+            aftOrder(root.rightChild);
+            System.out.print(root);
+        }
+    }
+
+
+    /**
+     * 非递归前序遍历
+     * @param root
+     */
+    public static void nonRecPreOrder(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode node = root;
+        while (node != null || stack.size() > 0) {
+            if (node != null) {
+                System.out.print(node);
+                stack.push(node);
+                node = node.leftChild;
+            } else if (stack.size() >0) {
+                node = stack.pop();
+                node = node.rightChild;
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * 非递归中序遍历
+     * @param root
+     */
+    public static void nonRecInOrder(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode node = root;
+        while (node != null || stack.size() > 0) {
+            //存在左子树
+            if (node != null) {
+                stack.push(node);
+                node = node.leftChild;
+                //栈非空
+            } else if (stack.size() >0) {
+                node = stack.pop();
+                System.out.print(node);
+                node = node.rightChild;
+            }
+        }
+        System.out.println();
+    }
+
+
+    /**
+     * 非递归后序遍历
+     * @param root
+     */
+    public static void nonRecAftOrder(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode node = root;
+        TreeNode p = root;
+        TreeNode preRight = p;
+        while (p != null) {
+            //将非叶子左子树压栈
+            for ( ; p.leftChild != null; p = p.leftChild) {
+                stack.push(p);
+            }
+            //当前节点没有右子树或者右子树已经输出
+            while (p != null && (p.rightChild == null || p.rightChild == preRight)) {
+                System.out.print(p);
+                preRight = p;
+                if (stack.isEmpty()) {
+                    return;
+                }
+                p = stack.pop();
+            }
+            //处理右子树
+            stack.push(p);
+            p = p.rightChild;
+        }
+        System.out.println();
+    }
+
+    /**
+     * 二叉树节点的数据结构类型
+     */
+    class TreeNode {
+        private int data;
+        private TreeNode leftChild;
+        private TreeNode rightChild;
+
+        public TreeNode() {
+        }
+
+        public TreeNode(int data) {
+            this.data = data;
+            leftChild = null;
+            rightChild = null;
+        }
+
+        @Override
+        public String toString() {
+            return " " + data;
+        }
+    }
+}
+
---
+++ java/src/com/winter/tree/HuffmanTree.java
@@ -0,0 +1,236 @@
+package com.winter.tree;
+
+import java.util.*;
+
+public class HuffmanTree {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//        System.out.print("请输入字符集大小n:");
+//        int n = scan.nextInt();
+//        HfTree[] ht = new HfTree[2*n-1];//存储字符节点及内部节点
+//
+//        for (int i = 0; i < n; i++) {
+//            ht[i] = new HfTree();
+//            System.out.print("请输入第" + (i+1) + "个字符:");
+//            ht[i].charset = scan.next().charAt(0);
+//            System.out.print("请输入第" + (i+1) + "个字符的权值:");
+//            ht[i].weight = scan.nextInt();
+//        }
+
+//        //构造哈夫曼树结构
+//        createHuffmanTree(ht,n);
+//        System.out.println("哈夫曼树结构:");
+//        for (int i = 0; i < 2*n-1; i++) {
+//            System.out.println(ht[i]);
+//        }
+//        System.out.println("各个字符编码");
+//        //生成哈夫曼编码
+//        HfCode[] hfCode = createHuffmanCode(ht,n);
+//        for (int i = 0; i < n; i++) {
+//            System.out.println("" + hfCode[i].charset + hfCode[i].code);
+//        }
+        System.out.print("请输入需要编码的数据:");
+        String str = scan.next();
+        //编码
+        coding(str);
+
+    }
+
+    /**
+     * 构造哈夫曼树
+     * @param ht
+     * @param n
+     */
+    public static void createHuffmanTree(HfTree[] ht, int n) {
+        for (int i = n; i < 2*n-1; i++) {
+            ht[i] = new HfTree();
+        }
+        for (int i = n; i < 2*n-1; i++) {
+            MyMin m = select(ht,i-1);
+            ht[i].weight = ht[m.min].weight + ht[m.nextMin].weight;
+            ht[i].leftChild = m.min;
+            ht[i].rightChild = m.nextMin;
+            ht[m.min].parent = i;
+            ht[m.nextMin].parent = i;
+        }
+    }
+
+    /**
+     * 生成哈夫曼编码
+     * @param ht
+     * @param n
+     * @return
+     */
+    public static HfCode[] createHuffmanCode(HfTree[] ht, int n) {
+        HfCode[] hfCode = new HfCode[n];
+        for (int i = 0; i < n; i++) {
+            hfCode[i] = new HfCode(ht[i].charset,i);
+            Deque code = new ArrayDeque();
+            int set = i;
+            int parent = ht[i].parent;
+            while (parent != -1) {
+                if (ht[parent].leftChild == set) {
+                    code.push("0");
+                } else {
+                    code.push("1");
+                }
+                set = parent;
+                parent = ht[parent].parent;
+            }
+            hfCode[i].code = code;
+        }
+        return hfCode;
+    }
+
+    /**
+     * 对输入的字符串编码
+     * @param str
+     * @return
+     */
+    public static void coding(String str) {
+        HashMap<Character,Integer> map = new HashMap();
+        for (int i = 0; i < str.length(); i++) {
+            if (map.containsKey(str.charAt(i))) {
+                map.put(str.charAt(i),map.get(str.charAt(i)) + 1);
+            } else {
+                map.put(str.charAt(i),1);
+            }
+        }
+        int n = map.size();
+        HfTree[] ht = new HfTree[2*n-1];
+        int i = 0;
+        Iterator<Map.Entry<Character,Integer>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Character,Integer> entry = it.next();
+            ht[i] = new HfTree();
+            ht[i].charset = entry.getKey();
+            ht[i].weight = entry.getValue();
+            i++;
+        }
+
+        createHuffmanTree(ht,n);
+        HfCode[] hfCode = createHuffmanCode(ht,n);
+        HashMap<Character,String> code = new HashMap<>();
+        for (int k = 0; k < hfCode.length; k++) {
+            Deque temp = hfCode[k].code;
+            StringBuilder sb = new StringBuilder();
+            while (!temp.isEmpty()) {
+                sb.append(temp.pop());
+            }
+            String cod = new String(sb);
+            code.put(hfCode[k].charset,cod);
+            System.out.println("" + hfCode[k].charset + "  : " + cod);
+        }
+        System.out.print("编码结果:");
+        for (int j = 0; j < str.length(); j++) {
+            char charset = str.charAt(j);
+            System.out.print(code.get(charset));
+        }
+
+    }
+
+
+    /**
+     * 返回最小值下标及次小值下标
+     * @param ht
+     * @param range
+     * @return
+     */
+    public static MyMin select(HfTree[] ht, int range) {
+        //默认下标为0
+        int min = 0;
+        //若0下标已被记录
+        if (ht[min].parent != -1) {
+            for (int i = 0; i <= range; i++) {
+                if (ht[i].parent == -1) {
+                    min = i;
+                    break;
+                }
+            }
+        }
+        //开始查找
+        for (int i = min + 1; i <= range; i++) {
+            if (ht[i].weight < ht[min].weight && ht[i].parent == -1) {
+                min = i;
+            }
+        }
+        //次小值默认从0开始
+        int nextMin = 0;
+            for (int i = 0; i <= range; i++) {
+                //不能是最小值且没有被记录
+                if (ht[i].parent == -1 && min != i) {
+                    nextMin = i;
+                    break;
+                }
+            }
+
+        for (int i = nextMin + 1; i <= range; i++) {
+            if (ht[i].weight < ht[nextMin].weight && ht[i].parent == -1
+                    &&  ht[i].weight >= ht[min].weight && min != i) {
+                nextMin = i;
+            }
+        }
+
+        MyMin m = new MyMin(min,nextMin);
+        return m;
+    }
+
+
+}
+
+/**
+ * 哈夫曼树每个节点的数据结构
+ */
+class HfTree {
+    char charset;
+    int weight;
+    int parent;
+    int leftChild;
+    int rightChild;
+
+    public HfTree() {
+        this.parent = -1;
+        this.leftChild = -1;
+        this.rightChild = -1;
+    }
+
+    @Override
+    public String toString() {
+        return " " + charset +
+                "," + weight +
+                "," + parent +
+                "," + leftChild +
+                "," + rightChild;
+    }
+}
+
+/**
+ * 哈夫曼编码的数据结构
+ */
+class HfCode {
+    char charset;
+    int index;
+    Deque code;
+
+    public HfCode() {
+    }
+
+    public HfCode(char charset, int index) {
+        this.charset = charset;
+        this.index = index;
+    }
+}
+
+/**
+ * 最小值与次小值的封装类
+ */
+class MyMin {
+    int min;
+    int nextMin;
+
+    public MyMin(int min, int nextMin) {
+        this.min = min;
+        this.nextMin = nextMin;
+    }
+}
